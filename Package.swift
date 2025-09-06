799cf85c20930138c47a39f9da37df599b38851d
Bryan Oppong-Boateng <bryan@bryanboateng.com>
2025-09-06 17:00:45 +0200

    Format files

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "anki-lyrics-note-generator",
-    targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
-        .executableTarget(
-            name: "anki-lyrics-note-generator"),
-    ]
+	name: "anki-lyrics-note-generator",
+	targets: [
+		// Targets are the basic building blocks of a package, defining a module or a test suite.
+		// Targets can depend on other targets in this package and products from dependencies.
+		.executableTarget(
+			name: "anki-lyrics-note-generator"),
+	]
 )
